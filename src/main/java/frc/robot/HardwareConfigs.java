cb43b941ad46e0ecfa6cc7e8a35cabc6719a2247
Joey4107 <joeygquick9113@gmail.com>
2024-12-19 20:54:36 -0600

    Initial commit

---
+++ src/main/java/frc/robot/HardwareConfigs.java
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+/** Add your docs here. */
+public final class HardwareConfigs {
+
+    // init
+
+    public SparkMaxConfig vortexConfig = new SparkMaxConfig();
+    public SparkMaxConfig neoConfig = new SparkMaxConfig();
+
+    public HardwareConfigs() {
+
+    }
+
+}
